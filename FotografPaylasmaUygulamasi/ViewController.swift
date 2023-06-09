a96d164783e180efbb32ad53ab3bc9f8e80a2ae4
exiw <51192517+exiw@users.noreply.github.com>
2023-06-10 00:16:28 +0300

    Initial Commit

---
+++ FotografPaylasmaUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FotografPaylasmaUygulamasi
+//
+//  Created by Emre Konukpay on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
