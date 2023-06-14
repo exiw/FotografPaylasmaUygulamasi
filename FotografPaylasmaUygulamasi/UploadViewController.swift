6df9688b13fa1c2f61346042b619656f99815b1c
exiw <51192517+exiw@users.noreply.github.com>
2023-06-14 09:14:32 +0300

    ikinci yüklemem ve firebase resim yüklemeyi ekledim

--- FotografPaylasmaUygulamasi/UploadViewController.swift
+++ FotografPaylasmaUygulamasi/UploadViewController.swift
@@ -6,15 +6,59 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var yorumTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
+    @objc func gorselSec(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func uploadButtonTiklandi(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data) { (storagemetadata, error) in
+                if error != nil{
+                    print(error?.localizedDescription)
+                }else{
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil{
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
    
 }
