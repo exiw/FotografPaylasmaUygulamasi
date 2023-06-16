bf951823d0cabd5ffacc1cc607f6dce9282db2dd
exiw <51192517+exiw@users.noreply.github.com>
2023-06-16 16:18:21 +0300

    Firestoredan veri çektim ve onları tableviewa aktardımn

---
+++ FotografPaylasmaUygulamasi/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  FotografPaylasmaUygulamasi
+//
+//  Created by Emre Konukpay on 16.06.2023.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
